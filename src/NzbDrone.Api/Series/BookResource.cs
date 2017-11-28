1c869401045b8f6ed3b3efcb2093cff594f6d34d
tingtom <tom_mathews@hotmail.co.uk>
2017-11-28 23:33:15 +0000

    New book resource and mapping

---
+++ src/NzbDrone.Api/Series/BookResource.cs
@@ -0,0 +1,63 @@
+﻿using NzbDrone.Api.REST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Api.Series
+{
+    public class BookGroupResource : RestResource
+    {
+        public List<BookResource> books;
+        
+        public class BookResource
+        {
+            public BookResource()
+            {
+
+            }
+
+            public string Title { get; set; }
+
+        }
+    }
+
+    public static class BookResourceMapper
+    {
+        public static BookGroupResource ToResource(this Core.Models.BookGroup group)
+        {
+            if (group == null) return null;
+
+            return new BookGroupResource() { books =
+                group.books.Select(a => new BookGroupResource.BookResource() {
+                    Title = a.Title
+
+                }).ToList()
+            };
+        }
+
+        public static Core.Models.BookGroup ToModel(this BookGroupResource resource)
+        {
+            if (resource == null) return null;
+
+            return new Core.Models.BookGroup
+            {
+                
+            };
+        }
+
+        public static Core.Models.BookGroup ToModel(this BookGroupResource resource, Core.Models.BookGroup group)
+        {
+            var updatedSeries = resource.ToModel();
+
+            group.ApplyChanges(updatedSeries);
+
+            return group;
+        }
+
+        public static List<BookGroupResource> ToResource(this IEnumerable<Core.Models.BookGroup> groups)
+        {
+            return groups.Select(ToResource).ToList();
+        }
+    }
+}
