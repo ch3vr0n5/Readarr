9a4d5639ff5606486cfba6ff070bf5440759474e
tingtom <tom_mathews@hotmail.co.uk>
2017-11-28 00:03:44 +0000

    Cleanup

--- src/NzbDrone.Common/Cloud/SonarrCloudRequestBuilder.cs
+++ src/NzbDrone.Common/Cloud/SonarrCloudRequestBuilder.cs
@@ -5,7 +5,6 @@ namespace NzbDrone.Common.Cloud
     public interface ISonarrCloudRequestBuilder
     {
         IHttpRequestBuilderFactory Services { get; }
-        IHttpRequestBuilderFactory SkyHookTvdb { get; }
         IHttpRequestBuilderFactory Books { get; }
     }
 
@@ -16,17 +15,11 @@ namespace NzbDrone.Common.Cloud
             Services = new HttpRequestBuilder("http://services.sonarr.tv/v1/")
                 .CreateFactory();
 
-            SkyHookTvdb = new HttpRequestBuilder("http://skyhook.sonarr.tv/v1/tvdb/{route}/{language}/")
-                .SetSegment("language", "en")
-                .CreateFactory();
-
             Books = new HttpRequestBuilder("https://www.googleapis.com/books/v1/volumes")
                 .CreateFactory();
         }
 
         public IHttpRequestBuilderFactory Services { get; }
-
-        public IHttpRequestBuilderFactory SkyHookTvdb { get; }
 
         public IHttpRequestBuilderFactory Books { get; }
     }
