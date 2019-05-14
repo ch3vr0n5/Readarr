df7a3839ea8b49a1e187dee2f369005dde9c3a17
tingtom <tom_mathews@hotmail.co.uk>
2019-05-14 23:00:14 +0100

    Remove monitor options, unsure what to do with this yet

--- frontend/src/Utilities/Series/monitorOptions.js
+++ frontend/src/Utilities/Series/monitorOptions.js
@@ -1,10 +1,10 @@
 const monitorOptions = [
   { key: 'all', value: 'All Episodes' },
-  { key: 'future', value: 'Future Episodes' },
-  { key: 'missing', value: 'Missing Episodes' },
-  { key: 'existing', value: 'Existing Episodes' },
-  { key: 'firstSeason', value: 'Only First Season' },
-  { key: 'latestSeason', value: 'Only Latest Season' },
+//   { key: 'future', value: 'Future Episodes' },
+//   { key: 'missing', value: 'Missing Episodes' },
+//   { key: 'existing', value: 'Existing Episodes' },
+//   { key: 'firstSeason', value: 'Only First Season' },
+//   { key: 'latestSeason', value: 'Only Latest Season' },
   { key: 'none', value: 'None' }
 ];
 
